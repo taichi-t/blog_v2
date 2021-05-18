1f48b21da04f32d82d3ae65e5cda088e0778e640
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-05-18 01:09:01 -0700

    recycle: Refactor code.

--- components/Markdown/CodeBlock.tsx
+++ components/Markdown/CodeBlock.tsx
@@ -2,27 +2,25 @@ import {
   CodeComponent,
   ReactMarkdownNames,
 } from 'react-markdown/src/ast-to-react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { css } from '@linaria/core';
-// import { PARSABLE_LANGUEAGES } from '@/constants/marked';
-// import { javascript } from 'react-syntax-highlighter/dist/cjs/languages/prism';
-// import { jsx } from 'react-syntax-highlighter/dist/cjs/languages/prism';
-// import { css as PrismCss } from 'react-syntax-highlighter/dist/cjs/languages/prism';
-// import { bash } from 'react-syntax-highlighter/dist/cjs/languages/prism';
-// import { json } from 'react-syntax-highlighter/dist/cjs/languages/prism';
-// import { typescript } from 'react-syntax-highlighter/dist/cjs/languages/prism';
-// import { tsx } from 'react-syntax-highlighter/dist/cjs/languages/prism';
-// import { markdown } from 'react-syntax-highlighter/dist/cjs/languages/prism';
+import { PARSABLE_LANGUEAGES } from '@/constants/marked';
+import { javascript } from 'react-syntax-highlighter/dist/cjs/languages/prism';
+import { jsx } from 'react-syntax-highlighter/dist/cjs/languages/prism';
+import { css as PrismCss } from 'react-syntax-highlighter/dist/cjs/languages/prism';
+import { bash } from 'react-syntax-highlighter/dist/cjs/languages/prism';
+import { json } from 'react-syntax-highlighter/dist/cjs/languages/prism';
+import { typescript } from 'react-syntax-highlighter/dist/cjs/languages/prism';
+import { tsx } from 'react-syntax-highlighter/dist/cjs/languages/prism';
 
-// SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.JAVASCRIPT, javascript);
-// SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.JSX, jsx);
-// SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.CSS, PrismCss);
-// SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.BASH, bash);
-// SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.JSON, json);
-// SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.TYPESCRIPT, typescript);
-// SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.TSX, tsx);
-// SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.MARKDOWN, markdown);
+SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.JAVASCRIPT, javascript);
+SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.JSX, jsx);
+SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.CSS, PrismCss);
+SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.BASH, bash);
+SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.JSON, json);
+SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.TYPESCRIPT, typescript);
+SyntaxHighlighter.registerLanguage(PARSABLE_LANGUEAGES.TSX, tsx);
 
 const CodeBlock: CodeComponent | ReactMarkdownNames = ({
   inline,
