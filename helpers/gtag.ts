e79f27d8b2e007f9ea490dda5a6e7aa110ef8720
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-06-12 17:53:19 -0700

    :sparkles: add gtag.

---
+++ helpers/gtag.ts
@@ -0,0 +1,35 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+type CategoryType = 'ui';
+
+type ClickEvent = {
+  action: 'click';
+  category: CategoryType;
+  label: string;
+  value: string;
+};
+
+export type Event = ClickEvent;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+const pageview = (url) => {
+  if (!GA_TRACKING_ID) return;
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+const event = ({ action, category, label, value }: Event) => {
+  if (!GA_TRACKING_ID) return;
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
+
+export default {
+  pageview,
+  event,
+};
--- hooks/useRouteEventHandler.ts
+++ hooks/useRouteEventHandler.ts
@@ -1,5 +1,6 @@
 import Router from 'next/router';
 import NProgress from 'nprogress';
+import gtag from '@/helpers/gtag';
 import * as React from 'react';
 
 const useRouteEventHandler = (): void => {
@@ -7,13 +8,19 @@ const useRouteEventHandler = (): void => {
     Router.events.on('routeChangeStart', () => {
       NProgress.start();
     });
-    Router.events.on('routeChangeComplete', () => NProgress.done());
+    Router.events.on('routeChangeComplete', (url) => {
+      NProgress.done();
+      gtag.pageview(url);
+    });
     Router.events.on('routeChangeError', () => NProgress.done());
     return () => {
       Router.events.off('routeChangeStart', () => {
         NProgress.start();
       });
-      Router.events.off('routeChangeComplete', () => NProgress.done());
+      Router.events.off('routeChangeComplete', (url) => {
+        NProgress.done();
+        gtag.pageview(url);
+      });
       Router.events.off('routeChangeError', () => NProgress.done());
     };
   }, []);
